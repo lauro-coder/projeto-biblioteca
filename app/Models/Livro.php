061e107b38a01f29ed8d4a7cbd9c320f30c5d02b
Lauro Santos <lauridammachado07@gmail.com>
2026-09-12 17:45:59 -0300

    models autor e livro

---
+++ app/Models/Livro.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Livro extends Model
+{
+    use HasFactory;
+
+    protected $table = 'livros';
+
+    protected $fillable = ['titulo', 'ano', 'autor_id'];
+
+    public function autor()
+    {
+        return $this->belongsTo(Autor::class);
+    }
+}
